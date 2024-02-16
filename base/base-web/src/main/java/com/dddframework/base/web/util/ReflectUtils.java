90384a83527bfc0a48ca143766f6a4c641500fe3
andyadc <jjwadc@gmail.com>
2024-02-16 09:33:31 +0800

    Project Initialization

---
+++ base/base-web/src/main/java/com/dddframework/base/web/util/ReflectUtils.java
@@ -0,0 +1,9 @@
+package com.dddframework.base.web.util;
+
+public final class ReflectUtils {
+
+    public static <T> Class<T> getSuperClassGenericType(final Class<?> clazz, final int index) {
+
+        return null;
+    }
+}
